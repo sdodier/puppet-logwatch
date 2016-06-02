7ad50526e69b1b60e9f22a44705276765e54b6a4
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-06-02 14:19:11 +0100

    Add support for managing ignore.conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,4 +10,16 @@ class logwatch::config inherits logwatch {
     content => template('logwatch/logwatch.conf.erb'),
   }
 
+  concat { 'ignore.conf':
+    path  => '/etc/logwatch/conf/ignore.conf',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
+  concat::fragment { 'header':
+    target  => 'ignore.conf',
+    content => "###### REGULAR EXPRESSIONS IN THIS FILE WILL BE TRIMMED FROM REPORT OUTPUT #####\n",
+    order   => '01',
+  }
 }
