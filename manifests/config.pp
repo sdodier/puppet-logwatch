0869a992b885f6ec1c7142988c6ffb43f2a3175c
Jon Mosco <jonny.mosco@gmail.com>
2017-05-11 22:36:06 -0400

    fix var scope

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,15 +10,15 @@ class logwatch::config inherits logwatch {
     content => template('logwatch/logwatch.conf.erb'),
   }
 
-  concat { $ignore_conf:
-    path  => $logwatch::params::ignore_conf,
+  concat { 'ignore.conf':
+    path  => $logwatch::ignore_conf,
     owner => 'root',
     group => 'root',
     mode  => '0644',
   }
 
   concat::fragment { 'header':
-    target  => $ignore_conf,
+    target  => 'ignore.conf',
     content => "###### REGULAR EXPRESSIONS IN THIS FILE WILL BE TRIMMED FROM REPORT OUTPUT #####\n",
     order   => '01',
   }
